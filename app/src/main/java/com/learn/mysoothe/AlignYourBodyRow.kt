38ff304a8aa5170e29e6ff3505a1042fdfe38211
Rusydy <rusydymuhiddin@gmail.com>
2024-02-18 17:42:52 +0700

    Add horizontal arrangement and content padding to AlignYourBodyRow

--- app/src/main/java/com/learn/mysoothe/AlignYourBodyRow.kt
+++ app/src/main/java/com/learn/mysoothe/AlignYourBodyRow.kt
@@ -2,6 +2,8 @@ package com.learn.mysoothe
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -16,7 +18,9 @@ fun AlignYourBodyRow(
     modifier: Modifier = Modifier
 ) {
     LazyRow(
-        modifier = modifier
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier,
     ) {
         items(alignYourBodyData) { item ->
             AlignYourBodyElement(
